17acadfaf4f513b8cb6aa8d862626f9c2a2e6a7c
oleglucky <iamrichestman@gmail.com>
2019-12-15 20:10:19 +0200

    done

---
+++ js/app.js
@@ -0,0 +1,43 @@
+console.log('ICAO with interpolation');
+let message = prompt('Enret 4 symbols:');
+
+const icao = {
+A: 'Alfa',
+b: 'Bravo',
+c: 'Charlie',
+d: 'Delta',
+e: 'Echo',
+f: 'Foxtrot',
+g: 'Golf',
+h: 'Hotel',
+i: 'India',
+j: 'Juliett',
+k: 'Kilo',
+l: 'Lima',
+m: 'Mike',
+n: 'November',
+o: 'Oscar',
+p: 'Papa',
+q: 'Quebec',
+r: 'Romeo',
+s: 'Sierra',
+t: 'Tango',
+u: 'Uniform',
+v: 'Victor',
+w: 'Whiskey',
+x: 'Xray',
+y: 'Yankee',
+z: 'Zulu',
+0: 'Zero',
+1: 'One',
+2: 'Two',
+3: 'Three',
+4: 'Four',
+5: 'Five',
+6: 'Six',
+7: 'Seven',
+8: 'Eight',
+9: 'Nine',
+}
+message.split('');
+console.log(`${icao[message[0]]} : ${icao[message[1]]} : ${icao[message[2]]} : ${icao[message[3]]}`);
